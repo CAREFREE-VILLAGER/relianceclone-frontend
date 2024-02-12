eb3206b14084c4bab93a42aef83ae6142d112651
CAREFREE-VILLAGER <as314202@gmail.com>
2024-02-12 23:37:21 +0530

    working on frontend and some changes

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -37,31 +38,60 @@ function Login() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-            required
+    <Box width={["95%", "90%", "90%", "90%"]} m="auto" mt={48} mb={12}>
+      <Flex
+        m="auto"
+        gap={["30px", "50px", "60px", "100px"]}
+        flexDirection={["column-reverse", "column-reverse", "column-reverse", "row"]}
+        alignItems="center"
+      >
+        <Box>
+          <Image
+            borderRadius={"15px"}
+            width={"800px"}
+            src="https://www.reliancedigital.in/akamai/images/mobile/Login-banner.jpeg"
+            alt="img"
           />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+        </Box>
+  
+        <Box
+          borderRadius={"15px"}
+          width={["90%", "90%", "90%", "40%"]}
+          boxShadow="rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px"
+        >
+          <Text p={4} borderBottom="1px solid #cecece" fontWeight={"semibold"}>
+            Login As User
+          </Text>
+  
+          <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label>Email:</label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  required
+                />
+              </div>
+              <div>
+                <label>Password:</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  required
+                />
+              </div>
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </Box>
+      </Flex>
+    </Box>
   );
+  
 }
 
 export default Login;
